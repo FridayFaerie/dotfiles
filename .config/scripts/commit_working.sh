e029d99c34d353cf17b9894afb659704cbca2da6
Friday <faeriethe@gmail.com>
2025-05-08 11:40:57 +0100

    added anims

---
+++ .config/scripts/commit_working.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+if [ -z "$1" ]; then
+  echo "you need a commit message :/"
+  exit 1
+fi
+
+git switch main &&
+
+git merge working --squash &&
+
+git commit -m "${1}" &&
+
+git switch working &&
+
+git merge main -m "merged from main"
