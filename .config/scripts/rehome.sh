70a6f7e9772b9e475b3e648c9dab1c72d3db6b35
Friday <faeriethe@gmail.com>
2025-05-11 10:57:38 +0100

    added bun's rehome

--- .config/scripts/rehome.sh
+++ .config/scripts/rehome.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
-cd /home/friday/config
+cd ~/config
 nix fmt ./*
 git add . &&
 git commit -m 'rebuilding home'
-nh home switch /home/friday/config
+
+if [[ "$HOME" == "friday" ]]; then
+  nh home switch ~/config
+else
+  home-manager switch --flake . --extra-experimental-features nix-command --extra-experimental-features flakes
+
