990b5afb8d34f7ddd59d10f5b8e43b52041fd860
Friday <faeriethe@gmail.com>
2025-10-20 23:20:11 +0100

    mah

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+function fish_greeting
+    # eval (ssh-agent -c)
+	clear
+end
+bind "/" expand-abbr or self-insert
+
+source ~/.config/scripts/alias.sh
+
+function starship_transient_prompt_func
+    starship module character
+end
+function starship_transient_rprompt_func
+    starship module directory
+    starship module time
+end
+starship init fish | source
+enable_transience
+
+zoxide init fish | source
+# if status is-interactive
+# end
