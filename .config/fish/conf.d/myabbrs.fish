24231c74122e648f855e7e16b81fb41ac07b5029
Friday <faeriethe@gmail.com>
2026-02-08 15:52:00 +0000

    hah

--- .config/fish/conf.d/myabbrs.fish
+++ .config/fish/conf.d/myabbrs.fish
@@ -1,11 +1,15 @@
 abbr -a --regex '^\\.\\.+$' --function multicd -- dotdot
-abbr -a --position anywhere -- qui /home/friday/.config/quickshell/
-abbr -a -- recon 'cd /home/friday/config'
-abbr -a -- smj 'quickshell -p /home/friday/projects/shimeji'
-abbr -a --position anywhere --command cd -- s /home/friday/projects/shimeji
-abbr -a -- r 'nix run nixpkgs/nixos-unstable#'
+# abbr -a -- smj 'quickshell -p /home/friday/projects/shimeji'
+# abbr -a --position anywhere --command cd -- s /home/friday/projects/shimeji
+# abbr -a -- r 'nix run nixpkgs/nixos-unstable#'
+# abbr -a --position anywhere --command nix -- n '--log-format internal-json -v &| nom --json'
 abbr -a -- n 'nvim'
 abbr -a -- cd 'z'
-abbr -a --position anywhere --command nix -- n '--log-format internal-json -v &| nom --json'
-abbr -a -- matlab 'matlab & disown && sleep 10; exit'
-abbr -a -- c 'nvim .config/'
+# abbr -a -- matlab 'matlab & disown && sleep 10; exit'
+abbr -a -- c 'nvim ~/.config/'
+abbr -a -- tp 'nvim -c TypstPreview'
+
+# Source - https://stackoverflow.com/a
+# Posted by Dave Powers
+# Retrieved 2026-01-26, License - CC BY-SA 4.0
+bind \cz 'fg 2>/dev/null; commandline -f repaint'
--- .config/scripts/battery.fish
+++ .config/scripts/battery.fish
@@ -1,12 +1,21 @@
 #!/usr/bin/env fish
+
+set timer 600
 while :
   set battery $(cat /sys/class/power_supply/BAT0/capacity)
+  set batteryStatus $(cat /sys/class/power_supply/BAT0/status)
 
-  if test $battery -le 20;
-    ffplay -autoexit -nodisp /usr/share/sounds/Oxygen-Sys-App-Error-Serious.ogg
-  else if test $battery -ge 90;
+  if test $battery -le 20 && string match -v $batteryStatus "Charging";
+    notify-send -u critical -t 5000 "Low Battery" "Please charge!"
+    ffplay -autoexit -nodisp /usr/share/sounds/Oxygen-Im-Nudge.ogg
+    set timer 60
+  else if test $battery -ge 90 && string match -v $batteryStatus "Discharging";
+    notify-send -u normal -t 5000 "High Battery" "Please disconnect!"
     ffplay -autoexit -nodisp /usr/share/sounds/Oxygen-Sys-App-Error.ogg
+    set timer 60
+  else
+    set timer 600
   end
 
-  sleep 600
+  sleep $timer
 end
